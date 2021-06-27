a0fad92e5bf724aed44557ab4656983425295c96
Kyoko Caraway <kcaraway@va-c1.kzn.21-school.ru>
2021-06-27 19:01:44 +0300

    add setCGIEnv

--- Response.cpp
+++ Response.cpp
@@ -18,16 +18,30 @@ void	Response::initialization(Request &req) {
 }
 
 void	Response::setCGIEnv() {
-	// this->cgi_env.insert(req.conf.cgiEnv.begin(), req.conf.cgiEnv.end());
-	std::map<std::string, std::list<std::pair<std::string, double> > >::iterator it = this->req.getHeaders().begin();
-	for (; it != this->req.getHeaders().end(); ++it) {
-		if (it->first.find("Secret") != std::string::npos) {
-			this->cgi_env[it->first] = it->second.begin()->first;
-		}
+	this->cgi_env.clear();
+	this->cgi_env["AUTH_TYPE"];
+	this->cgi_env["CONTENT_LENGTH"];
+	if (this->req.getHeaders().find("Content-Length") != this->req.getHeaders().end()) {
+		this->cgi_env["CONTENT_LENGTH"] = this->req.getHeaders().find("Content-Length")->second.begin()->first;
 	}
-	for (std::map<std::string, std::string>::iterator it1 = this->cgi_env.begin(); it1 != this->cgi_env.end(); ++it1) {
-		std::cout << it1->first << "=" << it1->second << std::endl;
+	this->cgi_env["CONTENT_TYPE"];
+	if (this->req.getHeaders().find("Content-Type") != this->req.getHeaders().end()) {
+		this->cgi_env["CONTENT_TYPE"] = this->req.getHeaders().find("Content-Type")->second.begin()->first;
 	}
+	this->cgi_env["GATEWAY_INTERFACE"] = "CGI/1.1";
+	this->cgi_env["PATH_INFO"]; // config(maybe request)
+	this->cgi_env["PATH_TRANSLATED"]; // full PATH_INFO
+	this->cgi_env["QUERY_STRING"] = this->req.getQuery();
+	this->cgi_env["REMOTE_ADDR"]; // config (ip host)
+	this->cgi_env["REMOTE_IDENT"]; // NO
+	this->cgi_env["REMOTE_USER"]; // NO
+	this->cgi_env["REQUEST_METHOD"] = this->req.getMethod();
+	this->cgi_env["REQUEST_URI"] = this->req.getPath();
+	this->cgi_env["SCRIPT_NAME"]; //config
+	this->cgi_env["SERVER_NAME"]; // config
+	this->cgi_env["SERVER_PORT"]; // config
+	this->cgi_env["SERVER_PROTOCOL"] = this->req.getVersion();
+	this->cgi_env["SERVER_SOFTWARE"] = this->req.getVersion();
 }
 
 
